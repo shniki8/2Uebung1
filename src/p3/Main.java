768ed25db86aad358eced108d81baa06e9d71132
Niklas <menkniklas@gmail.com>
2026-04-24 22:30:42 +0200

    Added shapes to reality. Even gods can make spelling mistakes.

--- src/p3/Main.java
+++ src/p3/Main.java
@@ -2,6 +2,21 @@ package p3;
 
 public class Main {
     public static void main(String[] args) {
+        Shape s = createShape('c',69,727); //playing god
+        System.out.println(s.getArea());
+        s = createShape('r', 69, 727);
+        System.out.println(s.getArea());
 
+    }
+
+    public static Shape createShape(char type, double a, double b){
+    if (type == 'c'){
+        return new Circle(a);
+    } else if (type == 'r') {
+        return new Rectangle(a,b);
+    }
+    else //user error, not my problem ¯\_(ツ)_/¯
+        System.out.println("Wenn c oder r zu schwer ist, wirst du bestimmt viel Spaß mit diesem null-Object haben!");
+        return null;
     }
 }
